d74f8320bd3890f53e7d3e660fdcb34bb38e872f
Crystal0109 <parkfam04@nate.com>
2025-11-02 22:08:49 +0900

    최신 payload

--- hanspell/spell_checker.py
+++ hanspell/spell_checker.py
@@ -47,11 +47,11 @@ def check(text):
         return Checked(result=False)
 
     payload = {
-        # 마지막 수정: 20251028 1537
-        'passportKey': 'c2fd711954ebef1e5d2039ad8c63e7a0a32c55b7',
+        # 마지막 수정: 20251102 2208
+        'passportKey': '0e298ae2463adce1afa7165dbb87fbd5c79b19dd',
         'where': 'nexearch',
         'color_blindness': 0,
-        '_': 1761633379815,
+        '_': 1762088884899,
         'q': text
     }
 
